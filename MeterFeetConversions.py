ce14234ff0e1be63212904092dfff3c567eeab9c
Layne Burkhead <burkheadlh@gmail.com>
2025-02-28 23:56:55 -0500

    Add files via upload

---
+++ MeterFeetConversions.py
@@ -0,0 +1,8 @@
+def feetToMeters(feet):
+	return (feet * 0.3048)
+
+
+def metersToFeet(meters):
+	return (meters / 0.3048)
+
+	
---
+++ SalesTax.py
@@ -0,0 +1,5 @@
+def salesTax(total):
+	return round((total * 0.06), 2)
+
+def totalWithTax(total, tax):
+	return round((total + tax), 2)
